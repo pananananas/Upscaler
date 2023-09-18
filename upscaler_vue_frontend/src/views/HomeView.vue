126aae3ee680db8dae09ecb9f96043f26851b7c0
pananananas <101266606+pananananas@users.noreply.github.com>
2023-09-18 11:10:18 +0200

    experimenting with styling

--- upscaler_vue_frontend/src/views/HomeView.vue
+++ upscaler_vue_frontend/src/views/HomeView.vue
@@ -1,11 +1,56 @@
 <template>
+<div className="overflow-hidden bg-[#1a1a1a] relative flex flex-row gap-4 w-full items-end"  id="LoadPhotoRoot">
+
+  <svg
+    class="relative overflow-visible"
+    style=""
+    width="795"
+    height="593"
+    viewBox="0 0 795 593"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg">
+    <path d="M424 428C311.5 529 62.5 438.5 0 577V-3.8147e-06L791 0C791 0 691.5 4.50001 620 117C548.5 229.5 536.5 327 424 428Z" fill="#054049"/>
+  </svg>
+
+  <div class="text-6xl font-port-lligat-slab leading-[50px] text-white w-full">
+    Upscale your
+    <br />
+    <div class="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600" id="UpscaleYourImages">
+      images
+    </div>
+  </div>
+
+
+
+
+
+
+
   <div>
     <input type="file" ref="fileInput" @change="uploadImage">
-    <button @click="sendToBackend">Prześlij do backendu</button>
   </div>
+</div>
+
+<svg>
+    <filter id="grainy">
+      <feTurbulence type="fractalNoise" baseFrequency="0.01" />
+      <feComposite operator="in" in2="SourceGraphic" />
+
+    </filter>
+
+    <rect x="0.5" y="0.5" width="415" height="281" rx="9.5" fill="#054049" fill-opacity="0.9" stroke="#6D6D6D" stroke-dasharray="6 4"/>
+</svg>
+
+
+
+
+
+
+
 </template>
 
-<script>
+
+<script lang="ts">
 export default {
   data() {
     return {
@@ -13,10 +58,9 @@ export default {
     };
   },
   methods: {
-    uploadImage(event) {
+    async uploadImage(event: { target: { files: null[]; }; }) {
+      
       this.selectedFile = event.target.files[0];
-    },
-    async sendToBackend() {
       if (!this.selectedFile) return;
 
       const formData = new FormData();
@@ -38,40 +82,11 @@ export default {
 };
 </script>
 
+<style>
+/* .container{
+  filter: url(#grainy);
+} */
 
 
-
-
-
-
-
-
-
-
-
-
-<!-- <template>
-  <div class="flex flex-wrap">
-    <div class="font-port-lligat-sans text-5xl">
-      This text is in Port Lligat Sans.
-    </div>
-
-    <div class="font-port-lligat-slab text-5xl">
-      This text is in Port Lligat Slab.
-    </div>
-  </div>
-</template>
-
-
-<script lang="ts">
-
-
-import { defineComponent } from 'vue';
-
-export default defineComponent({
-  name: 'HomeView',
-  components: {
-
-  },
-});
-</script> -->
+</style>
+```
