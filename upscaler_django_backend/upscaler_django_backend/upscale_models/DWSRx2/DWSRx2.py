8bb7286dfc868e1881a7682dabb9e490bce37bcb
pananananas <101266606+pananananas@users.noreply.github.com>
2023-10-08 20:57:55 +0200

    trying to fix finalcolorsr.py but failed miserably

--- upscaler_django_backend/upscaler_django_backend/upscale_models/DWSRx2/DWSRx2.py
+++ upscaler_django_backend/upscaler_django_backend/upscale_models/DWSRx2/DWSRx2.py
@@ -9,7 +9,7 @@ import pywt as pw
 import ntpath
 
 TEST_EXPERIMENT = True
-TEST_MODEL_PATH = 'upscaler_django_backend/upscale_models/DWSRx2/Weightx2/x2.ckpt'
+MODEL_PATH = 'upscaler_django_backend/upscale_models/DWSRx2/Weightx2/x2.ckpt'
 WV = 'db1'
 
 
@@ -21,37 +21,35 @@ def process_image(input_image_path, output_dir):
     with tf.Session(config=tf.ConfigProto()) as sess:
         tf.initialize_all_variables().run()
         saver = tf.train.Saver(tf.all_variables())
-        if TEST_EXPERIMENT:
-            saver.restore(sess, TEST_MODEL_PATH)
 
-            print('Processing Image %s' % ntpath.basename(input_image_path))
-            testBBImg = cv2.imread(input_image_path, 0)
-            tcoeffs = pw.dwt2(testBBImg, WV)
-            tcA, (tcH, tcV, tcD) = tcoeffs
-            tcA = tcA.astype(np.float32) / 255
-            tcH = tcH.astype(np.float32) / 255
-            tcV = tcV.astype(np.float32) / 255
-            tcD = tcD.astype(np.float32) / 255
-            test_temp = np.array([tcA, tcH, tcV, tcD])
-            test_elem = np.rollaxis(test_temp, 0, 3)
-            test_data = test_elem[np.newaxis, ...]
-            start_time = time.time()
-            output_data = sess.run([test_output], feed_dict={test_input: test_data})
-            duration = time.time() - start_time
-            dcA = output_data[0][0, :, :, 0]
-            dcH = output_data[0][0, :, :, 1]
-            dcV = output_data[0][0, :, :, 2]
-            dcD = output_data[0][0, :, :, 3]
-            srcoeffs = (dcA * 255 + tcA * 255,
-                        (dcH * 255 + tcH * 255,
-                         dcV * 255 + tcV * 255,
-                         dcD * 255 + tcD * 255))
-            sr_img = pw.idwt2(srcoeffs, WV)
+        saver.restore(sess, MODEL_PATH)
 
-            output_image_path = os.path.join(output_dir, ntpath.basename(input_image_path))
-            cv2.imwrite(output_image_path, sr_img)
-            print('Processed image saved to %s, processing time: %s' % (output_image_path, str(duration)))
-        else:
-            print('>>>>>>>>>Wrong script for New Experiment!')
+        print('Processing Image %s' % ntpath.basename(input_image_path))
+        testBBImg = cv2.imread(input_image_path, 0)
+        tcoeffs = pw.dwt2(testBBImg, WV)
+        tcA, (tcH, tcV, tcD) = tcoeffs
+        tcA = tcA.astype(np.float32) / 255
+        tcH = tcH.astype(np.float32) / 255
+        tcV = tcV.astype(np.float32) / 255
+        tcD = tcD.astype(np.float32) / 255
+        test_temp = np.array([tcA, tcH, tcV, tcD])
+        test_elem = np.rollaxis(test_temp, 0, 3)
+        test_data = test_elem[np.newaxis, ...]
+        start_time = time.time()
+        output_data = sess.run([test_output], feed_dict={test_input: test_data})
+        duration = time.time() - start_time
+        dcA = output_data[0][0, :, :, 0]
+        dcH = output_data[0][0, :, :, 1]
+        dcV = output_data[0][0, :, :, 2]
+        dcD = output_data[0][0, :, :, 3]
+        srcoeffs = (dcA * 255 + tcA * 255,
+                    (dcH * 255 + tcH * 255,
+                        dcV * 255 + tcV * 255,
+                        dcD * 255 + tcD * 255))
+        sr_img = pw.idwt2(srcoeffs, WV)
 
+        output_image_path = os.path.join(output_dir, ntpath.basename(input_image_path))
+        cv2.imwrite(output_image_path, sr_img)
+        print('Processed image saved to %s, processing time: %s' % (output_image_path, str(duration)))
+        
         sess.close()
--- upscaler_django_backend/upscaler_django_backend/upscale_models/DWSRx2/FinalColorSR.py
+++ upscaler_django_backend/upscaler_django_backend/upscale_models/DWSRx2/FinalColorSR.py
@@ -19,36 +19,47 @@ def generate_color_sr(input_dir, lum_dir, output_dir, scale=2):
     # Get the list of all files in the input directory
     input_images = glob.glob(os.path.join(input_dir, '*.png'))
 
+    BICTESTDATA_bud = glob.glob(os.path.join(BICTESTDATA_PATH, '*.png'))
+
     print(f'Start to generate color SR SCALE {scale}')
     print(f'Luminance SR from {lum_dir}')
     print(f'Cb Cr channel from {input_dir}')
     print(f'Final color SR store to {output_dir}')
 
-    # Loop through all images
-    for img_idx, input_image_path in enumerate(input_images):
-        input_image_name = os.path.basename(input_image_path)
-        print(f'Image: {img_idx + 1} | {input_image_name}')
-        
-        # Read bicubic down-scaled image
-        bic_test_img = cv2.imread(input_image_path)
-        
-        # Upscale the bicubic down-scaled image
-        bic_bic_test_img = cv2.resize(bic_test_img, (bic_test_img.shape[1]*scale, bic_test_img.shape[0]*scale), interpolation=cv2.INTER_CUBIC)
-        
-        # Convert to YCrCb color space
-        test_img_ycbcr = cv2.cvtColor(bic_bic_test_img, cv2.COLOR_BGR2YCrCb)
-        
-        # Set the Y channel to zero
-        test_img_ycbcr[:,:,0] = 0
-        
-        # Read the super-resolved luminance image
-        sr_img = cv2.imread(os.path.join(lum_dir, input_image_name), cv2.IMREAD_GRAYSCALE)
-        
-        # Replace the Y channel with the super-resolved image
-        test_img_ycbcr[:,:,0] = sr_img
-        
-        # Convert back to BGR color space
-        sr_color = cv2.cvtColor(test_img_ycbcr, cv2.COLOR_YCrCb2BGR)
-        
-        # Write the final super-resolved color image
-        cv2.imwrite(os.path.join(output_dir, input_image_name), sr_color)
+
+    bic_test_file_name = os.path.basename(input_dir)
+
+    
+    # Read bicubic down-scaled image
+    bic_test_img = cv2.imread(lum_dir)
+    
+    # Upscale the bicubic down-scaled image
+    bic_bic_test_img = cv2.resize(bic_test_img, (bic_test_img.shape[1]*scale, bic_test_img.shape[0]*scale), interpolation=cv2.INTER_CUBIC)
+    
+    # Convert to YCrCb color space
+    test_img_ycbcr = cv2.cvtColor(bic_bic_test_img, cv2.COLOR_BGR2YCrCb)
+    
+    # Set the Y channel to zero
+    test_img_ycbcr[:,:,0] = 0
+    
+    # Read the super-resolved luminance image
+    sr_img = cv2.imread(os.path.join(lum_dir, bic_test_file_name), cv2.IMREAD_GRAYSCALE)
+    
+    # Replace the Y channel with the super-resolved image
+    test_img_ycbcr[:,:,0] = sr_img
+    
+    # Convert back to BGR color space
+    sr_color = cv2.cvtColor(test_img_ycbcr, cv2.COLOR_YCrCb2BGR)
+    
+    # Write the final super-resolved color image
+    cv2.imwrite(os.path.join(output_dir, bic_test_file_name), sr_color)
+
+
+
+input_image_path = '/images/comic.png'
+sr_lum_dir = '/images/greyscaleSR/comic.png'
+output_dir = '/images/output'
+
+fileName = os.path.basename(input_image_path)
+
+generate_color_sr(input_image_path, sr_lum_dir, output_dir, scale=2) 
